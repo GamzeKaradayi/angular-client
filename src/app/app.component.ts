2e537534e26c258b9d75c0dd62bf2bb6dc3a3dc1
DIR\irem.tekkanat <irem.tekkanat@accenture.com>
2018-02-23 11:00:49 +0300

    Enter to house functionality is moved from App component to Login component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,12 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  private name: string;
-  private surname: string;
-  private houses: string[] = ['Ev 1', 'Ev 2', 'Ev 3', 'Ev 4', 'Ev 5'];
 
-  /** 
-   * Event handler for login(giris) button
-   */
-  private enterHouse(): void {
-    alert('Eve hoş geldin ' + this.name + ' ' + this.surname + ' :)');
+  private isUserEnteredToHouse: boolean;
+
+  private userEnteredToHouse(isEntered: boolean) {
+    if (isEntered) {
+      this.isUserEnteredToHouse = true;
+    }
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  @Output() onLoginSuccessful = new EventEmitter<boolean>();
+
+  private name: string;
+  private surname: string;
+  private houses: string[] = ['Ev 1', 'Ev 2', 'Ev 3', 'Ev 4', 'Ev 5'];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  /** 
+   * Event handler for login(giris) button
+   */
+  private enterHouse(): void {
+    alert('Eve hoş geldin ' + this.name + ' ' + this.surname + ' :)');
+    this.onLoginSuccessful.next(true);
+  }
 }
